dfd688c94c696385082262149fb1aca637470ce4
Ben Trevor <btrevor@8thlight.com>
2014-07-04 09:55:28 -0500

    fix scss saving, stop trying to decompress .z

--- init.el
+++ init.el
@@ -27,6 +27,9 @@
   (setq confirm-nonexistent-file-or-buffer nil)           ; don't prompt to create a new file
   (define-key global-map (kbd "RET") 'newline-and-indent) ; indent after pressing 'enter'
   (setq set-mark-command-repeat-pop 1)                    ; can press C-space to cycle through mark ring (after pressing C-x C-space)
+  (setq-default scss-compile-at-save nil)                 ; don't try to compile scss files
+  (setq-default auto-compression-mode 0)                  ; don't try to decompress files (like ~/.z)
+
 
   (autoload 'zap-up-to-char "misc" "like 't' in vim" 'interactive)
   (autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
@@ -104,8 +107,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("c6faf8734bd04d303d9a272daef7a1c37a85597e2de3c006a4319ecf579821a0" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "7d4d00a2c2a4bba551fcab9bfd9186abe5bfa986080947c2b99ef0b4081cb2a6" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" default)))
- '(org-agenda-files (quote ("~/org/todo.org" "~/org/enova.org" "~/org/coding.org"))))
+ '(custom-safe-themes (quote ("c6faf8734bd04d303d9a272daef7a1c37a85597e2de3c006a4319ecf579821a0" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "7d4d00a2c2a4bba551fcab9bfd9186abe5bfa986080947c2b99ef0b4081cb2a6" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
